fdfe809f8b2a3c9946283ffa6aed435bbc469507
Mike Aparicio <peruvianidol@gmail.com>
2022-07-11 13:40:02 -0500

    first commit

---
+++ .storybook/main.js
@@ -0,0 +1,20 @@
+module.exports = {
+  stories: [
+    "../src/**/*.stories.mdx",
+    "../src/**/*.stories.@(js|jsx|ts|tsx|svelte)",
+  ],
+  staticDirs: ["../public"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-svelte-csf",
+    "@storybook/addon-interactions",
+  ],
+  features: {
+    postcss: false,
+  },
+  framework: "@storybook/svelte",
+  core: {
+    builder: "@storybook/builder-webpack4",
+  },
+};
