bd3d9a1a03dab3d4db61d7cf45d891fbe3942f13
Mike Aparicio <peruvianidol@gmail.com>
2022-07-12 10:16:46 -0500

    Add Task component

---
+++ src/components/Task.svelte
@@ -0,0 +1,47 @@
+<script>
+	import { createEventDispatcher } from 'svelte';
+
+	const dispatch = createEventDispatcher();
+
+	// event hander for Pin Task
+	function PinTask() {
+		dispatch('onPinTask', {
+			id: task.id,
+		});
+	}
+
+	// event handler for Archive Task
+	function ArchiveTask() {
+		dispatch('onArchiveTask', {
+			id: task.id,
+		});
+	}
+
+	// Task props
+	export let task = {
+		id: '',
+		title: '',
+		state: '',
+		updatedAt: new Date(2021, 0, 1, 9, 0),
+	};
+
+	// Reactive declaration (computed prop in other frameworks)
+	$: isChecked = task.state === 'TASK_ARCHIVED';
+</script>
+
+<div class="list-item {task.state}">
+	<label class="checkbox">
+		<input type="checkbox" checked="{isChecked}" disabled name="checked"/>
+		<span class="checkbox-custom" on:click={ArchiveTask} aria-label="{`archiveTask-${task.id}`}"></span>
+	</label>
+	<div class="title">
+		<input type="text" readonly value={task.title} placeholder="Input title"/>
+	</div>
+	<div class="actions">
+		{#if task.state !== 'TASK_ARCHIVED'}
+		<a href="/" on:click|preventDefault={PinTask}>
+			<span class="icon-star" aria-label={`pinTask-${task.id}`}></span>
+		</a>
+		{/if}
+	</div>
+</div>
